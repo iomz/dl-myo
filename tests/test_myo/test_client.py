7e1f6c8c1eac3a51d9fef8d1862b18e351f95b18
Iori Mizutani <iori.mizutani@gmail.com>
2023-06-20 22:24:04 +0200

    test: parameterize the values in-file

--- tests/test_myo/test_client.py
+++ tests/test_myo/test_client.py
@@ -1,28 +1,28 @@
-import logging
-
+import pytest
 from myo import MyoClient
-from myo.types import ClassifierEvent, FVData, IMUData, MotionEvent
 
-logger = logging.getLogger(__name__)
-
-
-class TestClient(MyoClient):
-    async def on_classifier_event(self, ce: ClassifierEvent):
-        logger.info(ce)
-
-    async def on_emg_data(self, emg):
-        logger.info(emg)
-
-    async def on_fv_data(self, fvd: FVData):
-        logger.info(fvd)
-
-    async def on_imu_data(self, imu: IMUData):
-        logger.info(imu)
-
-    async def on_motion_event(self, me: MotionEvent):
-        logger.info(me)
+# from myo.types import ClassifierEvent, FVData, IMUData, MotionEvent
 
 
-def test_client():
-    tc = TestClient()
-    assert tc._client is None
+# class Client(MyoClient):
+#     async def on_classifier_event(self, ce: ClassifierEvent):
+#         pass
+#
+#     async def on_emg_data(self, emg):
+#         pass
+#
+#     async def on_fv_data(self, fvd: FVData):
+#         pass
+#
+#     async def on_imu_data(self, imu: IMUData):
+#         pass
+#
+#     async def on_motion_event(self, me: MotionEvent):
+#         pass
+
+
+@pytest.mark.asyncio
+async def test_client():
+    mc = MyoClient()
+    assert mc._client is None
+    # await tc.
